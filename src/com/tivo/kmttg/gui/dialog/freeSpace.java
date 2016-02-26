681c3eab8aa304802bfe7c757f0186c1fe38d38e
davidblackledge <davidblackledge@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2016-02-26 07:23:59 +0000

    convert freeSpace to use .fxml and .properties files.  put style info in kmttg.css adjust project to show css folders adjust tivoTab's launch of freeSpace.

--- src/com/tivo/kmttg/gui/dialog/freeSpace.java
+++ src/com/tivo/kmttg/gui/dialog/freeSpace.java
@@ -1,13 +1,19 @@
 package com.tivo.kmttg.gui.dialog;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URL;
+import java.text.MessageFormat;
 import java.util.Hashtable;
+import java.util.ResourceBundle;
 import java.util.Stack;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
-import javafx.geometry.Side;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.chart.PieChart;
 import javafx.scene.control.Label;
@@ -15,13 +21,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import com.sun.javafx.charts.Legend;
 import com.tivo.kmttg.gui.table.TableUtil;
 import com.tivo.kmttg.gui.table.bitrateTable;
 import com.tivo.kmttg.main.config;
@@ -29,54 +31,66 @@ import com.tivo.kmttg.util.debug;
 import com.tivo.kmttg.util.log;
 import com.tivo.kmttg.util.string;
 
-public class freeSpace {
+public class freeSpace implements Initializable {
    private String tivoName = null;
    private Stage frame = null;
    private Stage dialog = null;
-   private TextField space = null;
+   @FXML private TextField space = null;
    private bitrateTable tab = null;
-   private Label totals1 = null;
-   private Label totals2 = null;
+   @FXML private Label totals1 = null;
+   @FXML private Label totals2 = null;
    private float disk_space = 0;
    private Hashtable<String,Hashtable<String,Double>> chanData = new Hashtable<String,Hashtable<String,Double>>();
    private Hashtable<String,Object> totalsData = new Hashtable<String,Object>();
-   private MyPie chart = null;
-   private String[] labels = {"Keep Until I Delete", "Keep Until Space Needed", "Suggestions", "Free Space"};
-   private Color[] colors = {Color.GREEN, Color.YELLOW, Color.ORANGE, Color.BLUE};
+   @FXML private PieChart chart = null;
+   @FXML private ScrollPane tabScroll = null;
    
-   class MyPie extends PieChart {
-      public Legend legend;
-      
-      public MyPie() {
-         super();
-         legend = (Legend) getLegend();
-      }
+   private ResourceBundle bundle;
+   
+   public static freeSpace display(String tivoName, Stage frame) {
+	   freeSpace result;
+	   
+	   try {
+		   FXMLLoader loader = new FXMLLoader(freeSpace.class.getResource(
+				   "freeSpace.fxml"));
+		   // this controller must be instantiated here to initialize key information.
+		   result = new freeSpace(tivoName, frame);
+		   loader.setController(result);
+		   
+		   ResourceBundle bundle = ResourceBundle.getBundle("com.tivo.kmttg.gui.dialog.freeSpace");
+		   loader.setResources(bundle);
+		   VBox content = loader.<VBox>load();
+
+		   // create and display dialog window
+		   Stage dialog = new Stage();
+		   dialog.initOwner(frame);
+		   dialog.setTitle(MessageFormat.format(bundle.getString("dialog_title"), tivoName));
+		   dialog.setScene(new Scene(content));
+		   config.gui.setFontSize(dialog.getScene(), config.FontSize);
+		   result.dialog = dialog;
+		   result.dialog.show();
+	   } catch (IOException e1) {
+		   final StringWriter sw = new StringWriter();
+		   final PrintWriter pw = new PrintWriter(sw, true);
+		   e1.printStackTrace(pw);
+		   config.gui.text_error(sw.getBuffer().toString());
+		   // TODO Auto-generated catch block
+		   e1.printStackTrace();
+		   result = null;
+	   }
+	   return result;
    }
-
-   class PieData {
-      ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
-      ObservableList<Color> pieChartColors = FXCollections.observableArrayList();
-
-      public void add(PieChart.Data data, Color color){
-         pieChartData.add(data);
-         pieChartColors.add(color);
-      }
-   }   
-  
-   public freeSpace(String tivoName, Stage frame) {
+   private freeSpace(String tivoName, Stage frame) {
       this.tivoName = tivoName;
       this.frame = frame;
-      init();
    }
    
-   private void init() {      
-      chart = new MyPie();
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+	   this.bundle = resources;
+	   
       chart.setPrefWidth(frame.getWidth());
-      chart.getStyleClass().add("piechart_shows");
-      chart.setTitle(tivoName + " Disk Space Usage");
-      chart.setPrefHeight(300);
-      chart.setLegendSide(Side.RIGHT);
-      chart.setLabelsVisible(false);
+      chart.setTitle(MessageFormat.format(bundle.getString("chart"),tivoName));
 
       // Populate dataset
       if ( ! setData() ) {
@@ -84,67 +98,32 @@ public class freeSpace {
          return;
       }
       
-      // Dialog configuration      
-      VBox content = new VBox();
-      content.setAlignment(Pos.CENTER);
-      content.setSpacing(1);
-            
-      // Row 1
-      HBox row1 = new HBox();
-      row1.setSpacing(5);
-      // Free space
-      Label space_label = new Label("Total Disk Space (GB):");
-      space = new TextField();
-      space.addEventHandler(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
-         public void handle(KeyEvent event) {
-            if (event.isControlDown())
-               return;
-            if( event.getCode() == KeyCode.ENTER ) {
-               setData();
-               updateLabels();
-               config.diskSpace.put(tivoName, getDiskSpace());
-               config.save();
-            }
-         }
-      });
-      space.setText(String.format("%.1f",getDiskSpace()));
+      space.setText(MessageFormat.format(bundle.getString("space"),getDiskSpace()));
       space.setTooltip(config.gui.getToolTip("total_disk_space"));
-      row1.getChildren().addAll(space_label, space);
-      
-      // totals
-      totals1 = new Label("");
-      totals1.setAlignment(Pos.CENTER_LEFT);
-      totals1.getStyleClass().add("piechart_shows");
-
-      totals2 = new Label("");
-      totals1.setAlignment(Pos.CENTER_LEFT);
-      totals2.getStyleClass().add("piechart_shows");
 
       updateLabels();
-      
-      // Build content layout
-      content.getChildren().addAll(row1, chart, totals1, totals2);
       
       // bitrateTable
       tab = new bitrateTable();
       tab.AddRows(chanData);
-      ScrollPane tabScroll = new ScrollPane(tab.TABLE);
-      tabScroll.setPrefHeight(200);
-      tabScroll.setFitToHeight(true);
-      tabScroll.setFitToWidth(true);
-      content.getChildren().add(tabScroll);
+      tabScroll.setContent(tab.TABLE);
      
-      // create and display dialog window
-      dialog = new Stage();
-      dialog.initOwner(frame);
-      dialog.setTitle(tivoName + " Disk Usage Analysis");
-      dialog.setScene(new Scene(content));
-      config.gui.setFontSize(dialog.getScene(), config.FontSize);
-      dialog.show();
       TableUtil.autoSizeTableViewColumns(tab.TABLE, true);
    }
    
-   public Boolean setData() {
+   @FXML private void spaceCB(final KeyEvent event) {
+       if (event.isControlDown())
+           return;
+        if( event.getCode() == KeyCode.ENTER ) {
+           setData();
+           updateLabels();
+           config.diskSpace.put(tivoName, getDiskSpace());
+           config.save();
+        }
+	   
+   }
+   
+   private Boolean setData() {
       // Init data to 0
       Hashtable<String,Float> data = new Hashtable<String,Float>();      
       data.put("suggestions", (float)0.0);
@@ -219,28 +198,18 @@ public class freeSpace {
       
       int numSets = data.size();
       String[] legendLabels = new String[numSets];
-      PieData dataset = new PieData();
+      ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
       String[] keys = {"kuid", "kusn", "suggestions", "free"};
       for (int i=0; i<keys.length; ++i) {
-         legendLabels[i] = String.format(
-            "%s: %.2f GB (%.1f%%)",
-            labels[i], data.get(keys[i]), data.get(keys[i])*100/available
+         legendLabels[i] = MessageFormat.format(
+            bundle.getString(keys[i]+"_label"),
+            data.get(keys[i]), data.get(keys[i])*100/available
          );
-         dataset.add(new PieChart.Data(legendLabels[i],data.get(keys[i])), colors[i]);
+         pieChartData.add(new PieChart.Data(legendLabels[i],data.get(keys[i])));
       }
       
       // Update Pie Chart data
-      chart.setData(dataset.pieChartData);
-      
-      // Update pie colors to be what we want
-      for (PieChart.Data d : dataset.pieChartData) {
-         int idx = dataset.pieChartData.indexOf(d);
-         Color color = dataset.pieChartColors.get(idx);
-         d.getNode().setStyle(
-            "-fx-pie-color: " + color.toString().replace("0x", "#") + ";"
-         );
-         chart.legend.getItems().get(idx).setSymbol(new Rectangle(8, 8, color));
-     }
+      chart.setData(pieChartData);
       
       // Complete Totals data (recordings, bytes, duration set so far)
       totalsData.put("rate", bitrateTable.bitRate((Double)totalsData.get("bytes"),(Double)totalsData.get("duration")));
@@ -263,16 +232,13 @@ public class freeSpace {
    
    private void updateLabels() {
       totals1.setText(
-         "Recordings: " + totalsData.get("recordings") +
-         ", Space used: " + totalsData.get("bytes") + " GB" +
-         ", Total time: " + totalsData.get("duration")
+				MessageFormat.format(bundle.getString("totals1"), 
+				totalsData.get("recordings"), totalsData.get("bytes"), totalsData.get("duration"))
       );
       totals2.setText(
-         "Average Bit Rate: " + totalsData.get("rate") + " Mbps" +
-         ", Free Space: " + String.format("%.2f GB", (Float)totalsData.get("free")) +
-         ", Recording Time Remaining: " + totalsData.get("remaining")
+				MessageFormat.format(bundle.getString("totals2"), 
+				totalsData.get("rate"), (Float) totalsData.get("free"), totalsData.get("remaining"))
       );
-
    }
    
    private float getDiskSpace() {
@@ -302,7 +268,7 @@ public class freeSpace {
       debug.print("secs=" + secs);
       Integer hours = (int) (secs/3600);
       Integer mins  = (int) (secs/60 - hours*60);
-      return String.format("%02dh : %02dm", hours, mins);
+      return MessageFormat.format(bundle.getString("secsToHoursMins"), hours, mins);
    }  
    
    public void destroy() {
