a845399f50b79c7faf19a9702eba0115ea0bb22b
davidblackledge <davidblackledge@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2016-02-27 06:32:45 +0000

    convert ShowDetails to use .fxml and .properties files. adjust gui.java to use new method. ShowDetails changed to display stars instead of a number, and show single-named actors.

--- src/com/tivo/kmttg/gui/dialog/ShowDetails.java
+++ src/com/tivo/kmttg/gui/dialog/ShowDetails.java
@@ -2,19 +2,23 @@ package com.tivo.kmttg.gui.dialog;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.io.IOException;
+import java.net.URL;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -27,104 +31,75 @@ import com.tivo.kmttg.main.config;
 import com.tivo.kmttg.rpc.Remote;
 import com.tivo.kmttg.util.log;
 
-public class ShowDetails {
+public class ShowDetails implements Initializable {
+   private ResourceBundle bundle = null;
    private Stage dialog = null;
-   private Label mainTitle = null;
-   private Label subTitle = null;
-   private Label time = null;
-   private Label channel = null;
-   private Label description = null;
-   private Label otherInfo = null;
-   private Label actorInfo = null;
-   private Label image = null;
+   @FXML private Label mainTitle = null;
+   @FXML private Label subTitle = null;
+   @FXML private Label time = null;
+   @FXML private Label channel = null;
+   @FXML private Label description = null;
+   @FXML private Label otherInfo = null;
+   @FXML private Label actorInfo = null;
+   @FXML private Label image = null;
    private double x=-1, y=-1;
-   
-   public ShowDetails(Stage frame, JSONObject json) {
-      create(frame);
+
+   private static ShowDetails singleton;
+   public static ShowDetails load(Stage frame) {
+	   if(singleton == null) {
+		   singleton = create(frame);
+	   }
+	   return singleton;
    }
    
-   private void create(Stage frame) {
-      int minWidth = 400;
-      if (dialog == null) {
-         mainTitle = new Label("");
-         mainTitle.setMinWidth(minWidth);
-         mainTitle.setMaxWidth(minWidth);
-         mainTitle.setWrapText(true);
-         mainTitle.getStyleClass().add("show_details_title");
-         // Increase font size
-         mainTitle.setFont(
-            new Font(
-               mainTitle.getFont().getFamily(),
-               mainTitle.getFont().getSize()+5
-            )
-         );
-         
-         subTitle = new Label("");
-         subTitle.setMinWidth(minWidth);
-         subTitle.setMaxWidth(minWidth);
-         subTitle.setWrapText(true);
-         subTitle.getStyleClass().add("show_details_title");
-         
-         time = new Label("");
-         time.getStyleClass().add("show_details_other");
-         
-         channel = new Label("");
-         channel.getStyleClass().add("show_details_other");
-         
-         description = new Label("");
-         description.setMinWidth(minWidth);
-         description.setMaxWidth(minWidth);
-         description.setWrapText(true);
-         description.getStyleClass().add("show_details_text");
-         
-         otherInfo = new Label("");
-         otherInfo.setMinWidth(minWidth);
-         otherInfo.setMaxWidth(minWidth);
-         otherInfo.setWrapText(true);
-         otherInfo.getStyleClass().add("show_details_other");
-         
-         actorInfo = new Label("");
-         actorInfo.setMinWidth(minWidth);
-         actorInfo.setMaxWidth(minWidth);
-         actorInfo.setWrapText(true);
-         actorInfo.getStyleClass().add("show_details_actor");
-         
-         image = new Label("");
-         image.getStyleClass().add("show_details_title");
-         
-         // Start of layout management         
-         VBox left_panel = new VBox();
-         left_panel.setPadding(new Insets(0,0,0,3));
-         left_panel.getStyleClass().add("show_details_bg");
-         left_panel.setSpacing(2);
-         left_panel.getChildren().addAll(mainTitle, subTitle, time, channel, description, otherInfo, actorInfo);         
-         
-         VBox right_panel = new VBox();         
-         right_panel.setAlignment(Pos.CENTER);
-         right_panel.getStyleClass().add("show_details_bg");
-         right_panel.setSpacing(2);         
-         right_panel.getChildren().add(image);         
-         
-         HBox main_panel = new HBox();
-         main_panel.setSpacing(0);
-         main_panel.getChildren().addAll(left_panel, right_panel);
-         
+   private static ShowDetails create(Stage frame) {
+	   try {
+		   FXMLLoader loader = new FXMLLoader(configMain.class.getResource(
+				   "ShowDetails.fxml"));
+		   ResourceBundle bundle = ResourceBundle.getBundle("com.tivo.kmttg.gui.dialog.ShowDetails");
+		   loader.setResources(bundle);
+		   HBox content = loader.<HBox>load();
+		   
+		   // "initialize" fires here.
+		   
+		   Scene scene = new Scene(content);
+		   // save our official instance of configAuto
+		   ShowDetails controller = loader.<ShowDetails>getController();
+	      	  
+	     Stage dialog;
          dialog = new Stage();
+		 controller.dialog = dialog;
          dialog.setResizable(false);
-         dialog.setTitle("Show information");
+         dialog.setTitle(bundle.getString("dialog_title"));
          dialog.initOwner(frame);
          // This so we can restore original dialog position when re-opened
          dialog.setOnCloseRequest(new EventHandler<WindowEvent>() {
             @Override
             public void handle(WindowEvent arg0) {
-               x = dialog.getX(); y = dialog.getY();
+               singleton.x = dialog.getX(); singleton.y = dialog.getY();
             }
          });
-         Scene scene = new Scene(new VBox());
          config.gui.setFontSize(scene, config.FontSize);
-         ((VBox) scene.getRoot()).getChildren().add(main_panel);
          dialog.setScene(scene);
-      }
+         return controller;
+	   } catch (IOException e1) {
+		   // TODO Auto-generated catch block
+		   e1.printStackTrace();
+	   }
+	   return null;
+   }
+   
+   @Override
+   public void initialize(URL location, ResourceBundle resources) {
+	   bundle = resources;
+	   
+       // Increase font size
+       mainTitle.setFont(
+          new Font(
+             mainTitle.getFont().getFamily(),
+             mainTitle.getFont().getSize()+5
+          )
+       );
    }
    
    public void update(Node node, String tivoName, String recordingId) {
@@ -230,62 +205,64 @@ public class ShowDetails {
                   try {
                      // Title
                      String title = "";
-                     if (json.has("title"))
-                        title = json.getString("title");
-                     if (json.has("movieYear"))
-                        title += " (" + json.get("movieYear") + ")";
+                     // # 0:title, 1:intMovieYear
+//                     "{0}{1,choice,0#|0< ({1,number})}",
+                     title = MessageFormat.format(bundle.getString("title"),
+									json.optString("title"),
+									json.optInt("movieYear")
+                    		 );
                      mainTitle.setText(title);            
                      
                      // Subtitle (possibly with season & episode information)
                      String subtitle = "";
-                     if (json.has("subtitle"))
-                        subtitle = "\"" + json.getString("subtitle") + "\"";
-                     if (json.has("starRating"))
-                        subtitle += "Stars: " + starsToNum(json.getString("starRating"));
-                     if (json.has("seasonNumber") && json.has("episodeNum")) {
-                        subtitle += " (Sea " + json.get("seasonNumber") +
-                        " Ep " + json.getJSONArray("episodeNum").get(0) + ")";
-                     }
+                     // # 0:hasSubtitle? 1:subtitle, 2:floatStars, 3:intSeasonNum, 4:intEpisodeNum
+//                     "{0,choice,0#|1#\"{1}\"}{2,choice,0#|0< Stars: {2,number,#.#}}{3,choice,0#|0<{4,choice,0#,0< (Sea {3} Ep {4})}}",
+                     subtitle = MessageFormat.format(bundle.getString("subtitle"), 
+                     		json.has("subtitle")?1:0, json.optString("subtitle"),
+                     		starsToNum(json.optString("starRating")),
+                     		json.optInt("seasonNumber"), 
+                     		json.has("episodeNum")?json.getJSONArray("episodeNum").optInt(0):0
+                     		);
                      subTitle.setText(subtitle);
                      
                      // channel
                      String chan = "";
                      if (json.has("channel")) {
                         JSONObject c = json.getJSONObject("channel");
-                        if (c.has("channelNumber"))
-                        chan = c.getString("channelNumber");
-                        if (c.has("callSign"))
-                           chan += " " + c.getString("callSign");
+                        // # 0:hasChannelNumber? 1: channelNumber, 2: hasCallsign?, 3:callSign
+//                		"{0,choice,0#|1#{1}}{2,choice,0#|1# {3}}",
+                        chan = MessageFormat.format(bundle.getString("channel"),
+                        		c.has("channelNumber")?1:0, c.optString("channelNumber"), 
+                        		c.has("callSign")?1:0, c.optString("callSign"));
                      }
                      channel.setText(chan);
             
                      // time
                      String t = "";
-                     if (json.has("startTime")) {
-                        t = TableUtil.printableTimeFromJSON(json);
-                        if (json.has("duration")) {
-                           long s = TableUtil.getStartTime(json);
-                           long e = TableUtil.getEndTime(json);
-                           t += " (" + (int)Math.ceil((e-s)/60000.0) + " mins)";
-                        }
-                     }
+                     // # 0: hasStartTime? 1: startTime, 2: intDurationMins
+//            		 "{0,choice,0#|1#{1}{2,choice,0#|0< ({2} mins)}}", 
+                     t = MessageFormat.format(bundle.getString("time"),
+                    		 json.has("startTime")?1:0, json.has("startTime")?
+                    				 TableUtil.printableTimeFromJSON(json)
+                    				 :"",
+                    		 (json.has("startTime") && json.has("duration"))?
+                    				 (int)Math.ceil((TableUtil.getEndTime(json)-TableUtil.getStartTime(json))/60000.0)
+                    				 :0);
                      time.setText(t);
                         
                      // description
                      String desc = "";
-                     if (json.has("description")) {
-                        desc = json.getString("description");
-                        if (json.has("cc") && json.getBoolean("cc"))
-                           desc += " (CC)";
-                     }
+                     // 0: description 1: CC?
+//                     "{0}{1,choice,0#|1# (CC)}", 
+                     desc = MessageFormat.format(
+                    		 bundle.getString("description"),
+                    		 json.optString("description"),
+                    		 json.optBoolean("cc",false)?1:0);
                      description.setText(desc);
                      
                      // otherInfo
                      String other = "";
-                     if (json.has("mpaaRating"))
-                        other += "Rated " + json.getString("mpaaRating").toUpperCase() + "; ";
-                     else if (json.has("tvRating"))
-                        other += "TV " + json.getString("tvRating").toUpperCase() + "; ";
+                     String categories = "";
                      if (json.has("category")) {
                         JSONArray cat = json.getJSONArray("category");
                         Set<String> c = new HashSet<String>();
@@ -294,31 +271,50 @@ public class ShowDetails {
                               c.add(cat.getJSONObject(i).getString("label"));
                         }
                         for (String s : c)
-                           other += s + "; ";
+                           categories += s + "; ";
                      }
-                     if (json.has("hdtv") && json.getBoolean("hdtv"))
-                        other += "HD; ";
-                     if (json.has("originalAirdate")) {
-                        other += "First Aired: " + json.getString("originalAirdate") + "; ";
-                     }
-                     if (other.length() > 0)
+                     
+                     // # 0:hasmpaaRating? 1: mpaarating, 
+                     //   2:hastvRating? 3:tvRating, 
+                     //   4:hasCategory? 5:categorylist, 
+                     //   6:hdtv?, 7:originalAirDate? 8: originalAirDate
+//                     ""
+//            		 + "{0,choice,0#|1#Rated {1}; }"
+//            		 + "{2,choice,0#|1#TV {3}; }"
+//            		 + "{4,choice,0#|1#{5}}"
+//            		 + "{6,choice,0#|1#HD; }"
+//            		 + "{7,choice,0#|1#First Aired: {8}}"
+                     other = MessageFormat.format(bundle.getString("other"),
+                    		 json.has("mpaaRating")?1:0, json.optString("mpaaRating").toUpperCase(),
+                    		 json.has("tvRating")?1:0, json.optString("tvRating").toUpperCase(),
+                    		 json.has("category")?1:0, categories,
+                    		 json.optBoolean("hdtv",false)?1:0,
+                    		 json.has("originalAirdate")?1:0, json.optString("originalAirdate"));
+                     if (other.endsWith("; "))
                         other = other.substring(0, other.length()-2);
+                     
                      otherInfo.setText(other);
                      
                      // actorInfo
                      String actors = "";
                      if (json.has("credit")) {
-                        String separator = "";
                         int count = 0;
+                        String actorlist="";
+                        int actorcount = 0;
+                        String hostlist="";
+                        int hostcount = 0;
                         JSONArray credit = json.getJSONArray("credit");
                         // actors
                         for (int i=0; i<credit.length(); ++i) {
                            JSONObject a = credit.getJSONObject(i);
                            if (a.getString("role").equals("actor")) {
-                              if (a.has("first") && a.has("last")) {
-                                 if (count>0) separator = ", ";
-                                 actors += separator + a.getString("first") + " " + a.getString("last");
+                              if (a.has("first") || a.has("last")) {
+//                                // #0:first name, 1: last name, 2: actor number (1st = 1), 3: credit number (1st=1) 
+//                                actor="{3,choice,1#|1<, }{0} {1}"
+//                                ,actorfirst, actorlast, actorcount+1, count+1
+                            	 actorlist += MessageFormat.format(bundle.getString("actor"), a.optString("first",""), a.optString("last",""), actorcount+1, count+1);
                                  count++;
+                                 actorcount++;
                               }
                            }
                         }
@@ -337,14 +333,22 @@ public class ShowDetails {
                            for (int i=0; i<credit.length(); ++i) {
                               JSONObject a = credit.getJSONObject(i);
                               if (a.getString("role").equals("host")) {
-                                 if (a.has("first") && a.has("last")) {
-                                    if (count>0) separator = ", ";
-                                    actors += separator + a.getString("first") + " " + a.getString("last");
+                                 if (a.has("first") || a.has("last")) {
+//                                    actors += separator + a.getString("first") + " " + a.getString("last");
+//                                   // #0:first name, 1: last name, 2: host number (1st = 1), 3: credit number (1st=1) 
+//                                   host="{3,choice,1#|1<, }{0} {1}"
+//                                   ,hostfirst, hostlast, hostcount+1, count+1
+                                	 hostlist += MessageFormat.format(bundle.getString("host"), a.optString("first",""), a.optString("last",""), hostcount+1, count+1);
                                     count++;
+                                    hostcount++;
                                  }
                               }
                            }                     
                         }
+//                      // #0:actors, 1:actors total, 2:hosts, 3:hosts total, 4:overall total
+//                      actors = "{0}{1,choice,0#|0<{3,choice,0#|0<, }}{2}"
+//                      ,actorlist, actorcount, hostlist, hostcount, count
+                        actors = MessageFormat.format(bundle.getString("credits"), actorlist, actorcount, hostlist, hostcount, count);
                      }
                      actorInfo.setText(actors);
                      
@@ -380,7 +384,7 @@ public class ShowDetails {
       new Thread(task).start();
    }
    
-   private String starsToNum(String name) {
+   private double starsToNum(String name) {
       name = name.toLowerCase();
       name = name.replace("zero", "0");
       name = name.replace("one", "1");
@@ -389,7 +393,11 @@ public class ShowDetails {
       name = name.replace("four", "4");
       name = name.replace("five", "5");
       name = name.replace("point", ".");
-      return name;
+      try {
+    	  return Double.valueOf(name);
+      } catch(Exception e) {
+    	  return 0.0;
+      }
    }
    
    public Boolean isShowing() {
