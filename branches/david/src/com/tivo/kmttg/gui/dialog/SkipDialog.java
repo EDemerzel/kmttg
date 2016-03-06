a94caf1e8edb6d87e436769062f662fc910c1815
davidblackledge <davidblackledge@b0df3fdd-2057-4f73-b226-dfc9190e8f7a>
2016-03-06 04:49:26 +0000

    Merge 3924 from trunk to branch. First steps towards new GUI layout. Tooltips as an optional Help Pane (F1). Show Info as a basic pane with log output as alternate for Series 3. Job status collapsible, show window title status on Job status title, too. Ctrl+F for find(search) instead of Ctrl+S. Using "Shortcut" instead of "Ctrl" for more compatability.

--- branches/david/src/com/tivo/kmttg/gui/dialog/SkipDialog.java
+++ branches/david/src/com/tivo/kmttg/gui/dialog/SkipDialog.java
@@ -16,7 +16,7 @@ import com.tivo.kmttg.gui.MyTooltip;
 import com.tivo.kmttg.gui.table.TableUtil;
 import com.tivo.kmttg.gui.table.skipTable;
 import com.tivo.kmttg.main.config;
-import com.tivo.kmttg.rpc.SkipMode;
+import com.tivo.kmttg.rpc.AutoSkip;
 import com.tivo.kmttg.util.log;
 
 public class SkipDialog {
@@ -30,11 +30,11 @@ public class SkipDialog {
       getEntries();
    }
    
-   // Retrieve entries from SkipMode.ini file
+   // Retrieve entries from AutoSkip.ini file
    private void getEntries() {
       if (tab != null)
          tab.clear();
-      data = SkipMode.getEntries();      
+      data = AutoSkip.getEntries();      
       if (data != null && data.length() > 0) {
          if (dialog == null)
             init();
@@ -58,7 +58,7 @@ public class SkipDialog {
             try {
                for (int i=0; i<entries.length(); ++i) {
                   JSONObject json = entries.getJSONObject(i);
-                  SkipMode.removeEntry(json.getString("contentId"));
+                  AutoSkip.removeEntry(json.getString("contentId"));
                }
             } catch (Exception e) {
                log.error("removeEntries - " + e.getMessage());
@@ -77,7 +77,7 @@ public class SkipDialog {
 
       // Refresh button
       Button refresh = new Button("Refresh");
-      String tip = "<b>Refresh</b><br>Get list of SkipMode entries and refresh table.";
+      String tip = "<b>Refresh</b><br>Get list of AutoSkip entries and refresh table.";
       refresh.setTooltip(MyTooltip.make(tip));
       refresh.setOnAction(new EventHandler<ActionEvent>() {
          public void handle(ActionEvent e) {
@@ -85,20 +85,9 @@ public class SkipDialog {
          }
       });
 
-      // Change button
-      Button change = new Button("Change");
-      tip = "<b>Change</b><br>Update offsets for entries you changed in the table.<br>";
-      tip += "NOTE: The offset value is a time in milliseconds.";
-      change.setTooltip(MyTooltip.make(tip));
-      change.setOnAction(new EventHandler<ActionEvent>() {
-         public void handle(ActionEvent e) {
-            tab.changeTable();
-         }
-      });
-
       // Remove button
       Button remove = new Button("Remove");
-      tip = "<b>Remove</b><br>Remove selected entry in the table from SkipMode file.";
+      tip = "<b>Remove</b><br>Remove selected entry in the table from AutoSkip file.";
       remove.setTooltip(MyTooltip.make(tip));
       remove.setOnAction(new EventHandler<ActionEvent>() {
          public void handle(ActionEvent e) {
@@ -121,7 +110,7 @@ public class SkipDialog {
       // Row 1 = buttons
       HBox row1 = new HBox();
       row1.setSpacing(5);
-      row1.getChildren().addAll(refresh, change, remove);
+      row1.getChildren().addAll(refresh, remove);
       content.getChildren().add(row1);
       
       // Table
@@ -133,7 +122,7 @@ public class SkipDialog {
 
       dialog = new Stage();
       dialog.initOwner(frame);
-      dialog.setTitle("SkipMode Entries");
+      dialog.setTitle("AutoSkip Entries");
       Scene scene = new Scene(new VBox());
       config.gui.setFontSize(scene, config.FontSize);
       ((VBox) scene.getRoot()).getChildren().add(content);
--- branches/david/src/com/tivo/kmttg/task/comskip.java
+++ branches/david/src/com/tivo/kmttg/task/comskip.java
@@ -6,7 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.Hashtable;
 import java.util.Stack;
@@ -16,8 +15,8 @@ import java.util.regex.Pattern;
 import com.tivo.kmttg.main.config;
 import com.tivo.kmttg.main.jobData;
 import com.tivo.kmttg.main.jobMonitor;
+import com.tivo.kmttg.rpc.AutoSkip;
 import com.tivo.kmttg.rpc.SkipImport;
-import com.tivo.kmttg.rpc.SkipMode;
 import com.tivo.kmttg.util.backgroundProcess;
 import com.tivo.kmttg.util.debug;
 //import com.tivo.kmttg.util.ffmpeg;
@@ -224,26 +223,18 @@ public class comskip extends baseTask implements Serializable {
                // NOTE: Only delete a file ending in one of extensions
                if (f.endsWith(extensions[i]) && file.isFile(f)) file.delete(f);
             }
-            if (job.SkipPoint != null && file.isFile(job.edlFile)) {
-               // Analyze edl file
-               long closest = getClosestStart(job.edlFile, job.SkipPoint);
-               if (closest != -1)
-                  SkipMode.saveWithOffset(job.tivoName, job.contentId, job.offerId, job.title, closest);
-               else
-                  log.warn(job.title + ": Sorry, commercial point not found using automatic method - use z and pause method instead");
-            }
             if (job.SkipPoint != null) {
                String prefix = string.replaceSuffix(string.basename(job.mpegFile), "");
                file.cleanUpFiles(prefix);
             }
             
-            if (job.skipmode && config.VrdReview == 0 && config.comskip_review == 0 && file.isFile(job.edlFile)) {
+            if (job.autoskip && config.VrdReview == 0 && config.comskip_review == 0 && file.isFile(job.edlFile)) {
                // Skip table entry creation
                Stack<Hashtable<String,Long>> cuts = SkipImport.edlImport(job.edlFile, job.duration, false);
                if (cuts != null && cuts.size() > 0) {
-                  if (SkipMode.hasEntry(job.contentId))
-                     SkipMode.removeEntry(job.contentId);
-                  SkipMode.saveEntry(job.contentId, job.offerId, 0L, job.title, job.tivoName, cuts);
+                  if (AutoSkip.hasEntry(job.contentId))
+                     AutoSkip.removeEntry(job.contentId);
+                  AutoSkip.saveEntry(job.contentId, job.offerId, 0L, job.title, job.tivoName, cuts);
                }
                String prefix = string.replaceSuffix(string.basename(job.mpegFile), "");
                file.cleanUpFiles(prefix);               
@@ -251,31 +242,6 @@ public class comskip extends baseTask implements Serializable {
          }
       }
       return false;
-   }
-   
-   private long getClosestStart(String edlFile, String SkipPointStr) {
-      long closest = -1;
-      try {
-         Long SkipPoint = (long)Float.parseFloat(SkipPointStr);
-         BufferedReader ifp = new BufferedReader(new FileReader(edlFile));
-         String line = null;
-         float diff = SkipPoint;
-         while (( line = ifp.readLine()) != null) {
-            if (line.matches("^\\d+.+$")) {
-               String[] l = line.split("\\s+");
-               float start = Float.parseFloat(l[0])*1000;
-               if (Math.abs(start - SkipPoint) < diff) {
-                  diff = Math.abs(start - SkipPoint);
-                  closest = (long)start;
-               }
-            }
-         }
-         ifp.close();
-      } catch (Exception e) {
-         log.error("comskip getClosestStart - " + e.getMessage());
-         log.error(Arrays.toString(e.getStackTrace()));
-      }
-      return closest;
    }
    
    // Obtain pct complete from comskip stderr
